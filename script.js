ef475c7185e04062be7e218120445b3c344c9f83
makc9pa <2max2@mail.ru>
2021-11-06 00:20:04 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// const myFirstvar = 1
+
+// const myFunc = () => {
+//     console.log(myFirstvar)
+// }
+
+// myFunc()
+console.log("Hello World123")
+const test = 5
+console.log('test: ', test);
